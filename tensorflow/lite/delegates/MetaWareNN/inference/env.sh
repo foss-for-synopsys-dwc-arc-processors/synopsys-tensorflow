fd49dd57c6f0f5269a2fc48ea0d02b1bab85f6a5
Janani Sundaragandhi <janani.s@multicorewareinc.com>
2021-08-18 19:27:34 +0530

    MetaWareNN-Delegate: Update Comments

--- tensorflow/lite/delegates/MetaWareNN/inference/env.sh
+++ tensorflow/lite/delegates/MetaWareNN/inference/env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ########### Executable Networks flow ##############
-#set the path to onnxruntime
+#set the path to synopsys-tensorflow
 export FRAMEWORK_PATH=/Path/to/synopsys-tensorflow/
 export METAWARENN_LIB_PATH=$FRAMEWORK_PATH"/tensorflow/lite/delegates/MetaWareNN/builders/metawarenn_lib/"
 export EXEC_DUMPS_PATH=$FRAMEWORK_PATH"/EXEC_DUMPS/"
